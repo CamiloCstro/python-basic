54edce289b8205d8bbe834a1e03e46e91c724d60
camilo <camilocstr2020@gmail.com>
2021-09-13 19:20:41 -0500

    Primeras clases del curso

---
+++ 1.-Introduccion/constantes.py
@@ -0,0 +1,5 @@
+# Constantes no puede cambiar de valor en tiempo de ejecución
+# Se definen en mayusculas
+USER_NAME = 'Camilo'
+
+print(USER_NAME)
---
+++ 1.-Introduccion/variables.py
@@ -0,0 +1,16 @@
+# Etiquetas
+# nomenclatura snake_case unir palabras mediante un _ y todas las letras se escriben en minuscula
+
+# nombre de la variable descriptivo signo de = y su valor
+name_user = 'Camilo'
+last_name = 'Castro'
+age = 24
+weigh = 76.3
+height = 1.78
+gender_male = True
+
+print(name_user)
+
+# las variables pueden cambiar su valor en tiempo de ejecución
+name_user = 'Andres'
+print(name_user)
