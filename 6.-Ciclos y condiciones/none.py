ee932fcbcf8d0164cd21f24235509f16c79f2f0d
camilo <camilocstr2020@gmail.com>
2021-09-15 16:06:09 -0500

    uso de la palabra None

---
+++ 6.-Ciclos y condiciones/none.py
@@ -0,0 +1,19 @@
+# si no se cual es el tipo de mi variable puedo utilizar una varible vacia
+# None -> vacia
+
+# No utilizamos espacio en memoria
+var = None
+print(type(var))
+
+# True 1 / False 0
+
+''' Elemenos falsos
+False
+None
+0
+0.0
+'' / ""
+[]
+()
+{}
+'''
