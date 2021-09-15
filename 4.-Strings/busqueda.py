b3f80ff538339a3b7d4db3ba4856c2a3321c039e
camilo <camilocstr2020@gmail.com>
2021-09-14 22:47:30 -0500

    busqueda de caracteres en un string

---
+++ 4.-Strings/busqueda.py
@@ -0,0 +1,8 @@
+# serch strings in to other string
+
+tittle_curse = 'python curse curse'
+
+# Cuenta el numero de veces que aparece una secuencia de caracteres en un string
+print(tittle_curse.count('curse'))
+
+print('python' in tittle_curse)
