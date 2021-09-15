52c94428c2c4ffbb61ca9a142c819d5a0323486f
camilo <camilocstr2020@gmail.com>
2021-09-15 12:20:16 -0500

    imprimir llaves valores y los items de un diccionario

---
+++ 5.-Diccionarios/llaves.py
@@ -0,0 +1,16 @@
+diccionario = {'camilo': 24, 'Andres': 25, 'Juan': 32}
+
+# keys imprimir las llaves del diccionario
+for keys in diccionario:
+    print(keys)
+
+print(diccionario.keys())
+
+# values
+# imprime los valores del diccionario
+print(diccionario.values())
+
+# items
+# Se recomienda convertir a tuplas para que no se puedan modificar los valores.
+# imprime las llaves y valores del diccionario
+print(diccionario.items())
