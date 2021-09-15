39e302198e3121a58a1825fc58bc74dc3bba94bc
camilo <camilocstr2020@gmail.com>
2021-09-15 16:27:12 -0500

    Condicionales

---
+++ 6.-Ciclos y condiciones/condiciones.py
@@ -0,0 +1,18 @@
+# if
+
+result = 4
+
+if result == 10:
+    print('Soy el 10')
+else:
+    print('no soy 10')
+
+num_a = 0
+num_b = 100
+
+if num_a > num_b:
+    print(f'Soy {num_a} mayor que {num_b}')
+elif num_a == num_b:
+    print(f'Soy {num_a} igual que {num_b}')
+else:
+    print(f'Soy {num_a} menor que {num_b}')
