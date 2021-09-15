8bd38829661e2c508bc739db1054a68a2a4abfcd
camilo <camilocstr2020@gmail.com>
2021-09-15 16:48:54 -0500

    Condicionar variables

---
+++ 6.-Ciclos y condiciones/asignar.py
@@ -0,0 +1,14 @@
+
+var = '' or 'Andres'
+var_two = '' or [] or False
+print(var)
+print(var_two)
+
+var_one = ''
+var_three = 'soy true'
+
+# msj = 'True' if var_one == True or var_three == True else 'False'
+
+msj = var_one or var_three
+
+print(msj)
